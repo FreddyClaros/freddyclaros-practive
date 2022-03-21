b9317fa764ecf90e8f2482864ca52e6e7a2a1404
FreddyClaros <freddyjhonatanclaros@gmail.com>
2022-03-20 22:15:54 -0400

    Initial files

---
+++ test/tdd_test.py
@@ -0,0 +1,24 @@
+#
+# @tdd_practice.py Copyright (c) 2022 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# Edificio Union № 1376 Av. General Inofuentes esquina Calle 20, La Paz, Bolivia.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
+import unittest
+from src.tdd import Tdd
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
